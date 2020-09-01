c5baf94ae47ea988f576990382ccb55879a78fcc
Tomoyuki HIRAI <gotetu@gmail.com>
2020-09-01 23:46:02 +0900

    Initial commit

---
+++ src/main/java/com/github/gotetu/springangularshowcase/SpringAngularShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.github.gotetu.springangularshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularShowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/gotetu/springangularshowcase/SpringAngularShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gotetu.springangularshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
