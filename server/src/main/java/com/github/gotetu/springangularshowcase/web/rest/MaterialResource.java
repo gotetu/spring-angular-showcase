def52ed79947370476db051b2f4458ad598bbdb7
Tomoyuki HIRAI <gotetu@gmail.com>
2020-09-04 00:10:22 +0900

    WebAPIの作成.

---
+++ server/src/main/java/com/github/gotetu/springangularshowcase/web/rest/MaterialResource.java
@@ -0,0 +1,23 @@
+package com.github.gotetu.springangularshowcase.web.rest;
+
+import com.github.gotetu.springangularshowcase.domain.model.Material;
+import com.github.gotetu.springangularshowcase.service.MaterialService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/material")
+public class MaterialResource {
+    private final MaterialService service;
+    public MaterialResource(final MaterialService service) {
+        this.service = service;
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<Material>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+}
