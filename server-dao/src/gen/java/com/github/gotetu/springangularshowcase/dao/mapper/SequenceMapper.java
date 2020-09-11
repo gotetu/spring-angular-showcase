d905ccaac341f81d1134c916a712562524263754
Tomoyuki HIRAI <gotetu@gmail.com>
2020-09-12 00:27:53 +0900

    MyBatis GeneratorによるDAOの自動生成

---
+++ server-dao/src/gen/java/com/github/gotetu/springangularshowcase/dao/mapper/SequenceMapper.java
@@ -0,0 +1,10 @@
+package com.github.gotetu.springangularshowcase.dao.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface SequenceMapper {
+
+    @Select("SELECT ${sequenceName}.NEXTVAL")
+    long nextval(@Param("sequenceName") String sequenceName);
+}
